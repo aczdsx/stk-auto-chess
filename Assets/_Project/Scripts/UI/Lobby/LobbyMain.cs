c65c71ffc417a7fe966049ad6b18790c1ad0dcb5
JiyunHyun <jyhyun@cookapps.com>
2024-05-08 19:48:12 +0900

    [dev] 기본 로비 씬 로드 구현

--- Assets/_Project/Scripts/UI/Lobby/LobbyMain.cs
+++ Assets/_Project/Scripts/UI/Lobby/LobbyMain.cs
@@ -7,30 +7,10 @@ namespace CookApps.AutoBattler
     [RegisterUILayer(UILayerType.Cover, "Prefabs/UI/Lobby/LobbyMain.prefab")]
     public class LobbyMain : UILayer
     {
-        [SerializeField] private CAButton btnStart;
-
-        protected override void Awake()
-        {
-            base.Awake();
-            btnStart.onClick.AddListener(OnClickStart);
-        }
-
-        protected override void OnDestroy()
-        {
-            base.OnDestroy();
-            btnStart.onClick.RemoveListener(OnClickStart);
-        }
-
         public override void OnPreEnter(object param)
         {
             base.OnPreEnter(param);
             TopCurrencyAndMenuBar.AddToUILayer(this, TopPanelType.Bread, TopPanelType.Coin, TopPanelType.Jewel, TopPanelType.Menu);
-        }
-
-        private void OnClickStart()
-        {
-            int currentStageId = UserDataManager.Instance.GetCurrentStageId();
-            SceneUILayerManager.Instance.PushUILayerAsync<ChapterMain>(currentStageId).Forget();
         }
     }
 }
