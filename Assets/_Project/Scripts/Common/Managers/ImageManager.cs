761e732a1c78420d3b02b0d5a4218cde25878927
JiyunHyun <jyhyun@cookapps.com>
2024-05-20 20:54:00 +0900

    [dev] Defins 추가

--- Assets/_Project/Scripts/Common/Managers/ImageManager.cs
+++ Assets/_Project/Scripts/Common/Managers/ImageManager.cs
@@ -14,11 +14,11 @@ namespace CookApps.AutoBattler
                 // case Grade.COMMON:
                 //     return AtlasManager.Instance.GetSprite("UI_Main", "Icon_R");
                 case Grade.RARE:
-                    return AtlasManager.Instance.GetSprite("UI_Main", "Icon_R");
+                    return AtlasManager.Instance.GetSprite(Defines.UI_ATLAS_NAME, "Icon_R");
                 case Grade.EPIC:
-                    return AtlasManager.Instance.GetSprite("UI_Main", "Icon_SR");
+                    return AtlasManager.Instance.GetSprite(Defines.UI_ATLAS_NAME, "Icon_SR");
                 case Grade.LEGEND:
-                    return AtlasManager.Instance.GetSprite("UI_Main", "Icon_SSR");
+                    return AtlasManager.Instance.GetSprite(Defines.UI_ATLAS_NAME, "Icon_SSR");
                 default:
                     return null;
             }
@@ -29,17 +29,17 @@ namespace CookApps.AutoBattler
             switch (type)
             {
                 case CharacterType.FIRE:
-                    return AtlasManager.Instance.GetSprite("UI_Main", "Icon_Fire_Active");
+                    return AtlasManager.Instance.GetSprite(Defines.UI_ATLAS_NAME, "Icon_Fire_Active");
                 case CharacterType.WATER:
-                    return AtlasManager.Instance.GetSprite("UI_Main", "Icon_Water_Active");
+                    return AtlasManager.Instance.GetSprite(Defines.UI_ATLAS_NAME, "Icon_Water_Active");
                 case CharacterType.EARTH:
-                    return AtlasManager.Instance.GetSprite("UI_Main", "Icon_Ground_Active");
+                    return AtlasManager.Instance.GetSprite(Defines.UI_ATLAS_NAME, "Icon_Ground_Active");
                 case CharacterType.WIND:
-                    return AtlasManager.Instance.GetSprite("UI_Main", "Icon_Wind_Active");
+                    return AtlasManager.Instance.GetSprite(Defines.UI_ATLAS_NAME, "Icon_Wind_Active");
                 case CharacterType.LIGHT:
-                    return AtlasManager.Instance.GetSprite("UI_Main", "Icon_Light_Active");
+                    return AtlasManager.Instance.GetSprite(Defines.UI_ATLAS_NAME, "Icon_Light_Active");
                 case CharacterType.DARK:
-                    return AtlasManager.Instance.GetSprite("UI_Main", "Icon_Dark_Active");
+                    return AtlasManager.Instance.GetSprite(Defines.UI_ATLAS_NAME, "Icon_Dark_Active");
                 default:
                     return null;
             }
@@ -50,17 +50,17 @@ namespace CookApps.AutoBattler
             switch (position)
             {
                 case CharacterPosition.TANK:
-                    return AtlasManager.Instance.GetSprite("UI_Main", "Icon_Position_Tank_Active");
+                    return AtlasManager.Instance.GetSprite(Defines.UI_ATLAS_NAME, "Icon_Position_Tank_Active");
                 case CharacterPosition.WARRIOR:
-                    return AtlasManager.Instance.GetSprite("UI_Main", "Icon_Position_Warrior_Active");
+                    return AtlasManager.Instance.GetSprite(Defines.UI_ATLAS_NAME, "Icon_Position_Warrior_Active");
                 case CharacterPosition.RANGER:
-                    return AtlasManager.Instance.GetSprite("UI_Main", "Icon_Position_Sniper_Active");
+                    return AtlasManager.Instance.GetSprite(Defines.UI_ATLAS_NAME, "Icon_Position_Sniper_Active");
                 case CharacterPosition.WIZARD:
-                    return AtlasManager.Instance.GetSprite("UI_Main", "Icon_Position_Wizard_Active");
+                    return AtlasManager.Instance.GetSprite(Defines.UI_ATLAS_NAME, "Icon_Position_Wizard_Active");
                 case CharacterPosition.SUPPORTER:
-                    return AtlasManager.Instance.GetSprite("UI_Main", "Icon_Position_Supporter_Active");
+                    return AtlasManager.Instance.GetSprite(Defines.UI_ATLAS_NAME, "Icon_Position_Supporter_Active");
                 case CharacterPosition.ASSASSIN:
-                    return AtlasManager.Instance.GetSprite("UI_Main", "Icon_Position_Assassin_Active");
+                    return AtlasManager.Instance.GetSprite(Defines.UI_ATLAS_NAME, "Icon_Position_Assassin_Active");
                 default:
                     return null;
             }
