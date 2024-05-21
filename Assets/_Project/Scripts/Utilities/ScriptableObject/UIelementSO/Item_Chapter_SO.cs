2d71d984e4401d775b21325f62931f2639f359ba
Doyeople <160451574+Doyeople@users.noreply.github.com>
2024-05-21 18:45:58 +0900

    [UI] Item_chapter_SO 추가

---
+++ Assets/_Project/Scripts/Utilities/ScriptableObject/UIelementSO/Item_Chapter_SO.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using AYellowpaper.SerializedCollections;
+using UnityEngine;
+
+
+namespace CookApps.AutoBattler
+{
+    [CreateAssetMenu(fileName = "Item_Chapter_SO", menuName = "ScriptableObjects/UIelementData/Item_Chapter")]
+    public class Item_Chapter_SO : ScriptableObject
+    {
+        [SerializedDictionary("Name", "float")]
+        public SerializedDictionary<string, float> BgHeight;
+
+        [SerializedDictionary("Color Name", "Color")]
+        public SerializedDictionary<string, Color> TextColors;
+
+        [SerializedDictionary("Color Name", "Color")]
+        public SerializedDictionary<string, Color> FloorColors;
+
+        [SerializedDictionary("Color Name", "Color")]
+        public SerializedDictionary<string, Gradient> PillarColors;
+
+
+
+
+    }
+}
