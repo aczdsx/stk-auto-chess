501b0f72ac87ec6f2b5bdf7c0b8fb83a4858ebee
Doyeople <160451574+Doyeople@users.noreply.github.com>
2024-06-11 20:49:06 +0900

    [UI] 게이지 트윈 스크립트

--- Assets/_Project/Scripts/UI/Common/InGameRatioTween.cs
+++ Assets/_Project/Scripts/UI/Common/InGameRatioTween.cs
@@ -9,14 +9,14 @@ public class InGameRatioTween : MonoBehaviour
     public TMP_Text damageRatio;
     public Color defaultColor;
     public Color damageColor;
-    public Sequence DamageTween()
-    {
-        return DOTween.Sequence()
-            .Join(damageRatio.DOColor(damageColor, 0.2f).SetEase(Ease.OutQuad))
-            .Join(damageRatio.transform.DOScale(1.14f, 0.2f).SetEase(Ease.OutQuad))
-            .Append(damageRatio.DOColor(defaultColor, 0.2f).SetEase(Ease.InQuad))
-            .Append(damageRatio.transform.DOScale(1f, 0.2f).SetEase(Ease.InQuad));     
-    }
 
+    public void DamageFXTween()
+    {
+            damageRatio.DOColor(damageColor, 0.2f).SetEase(Ease.OutQuad);
+            damageRatio.DOColor(defaultColor, 0.2f).SetEase(Ease.OutQuad).SetDelay(0.2f);
+            damageRatio.transform.DOScale(1.4f, 0.2f).SetEase(Ease.OutQuad);
+            damageRatio.transform.DOScale(1f, 0.2f).SetEase(Ease.OutQuad).SetDelay(0.2f);
+    }
+    
 
 }
