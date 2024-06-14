de1ae34d6e71b945c1da1d17c7baf2f5cceda8ab
JiyunHyun <jyhyun@cookapps.com>
2024-06-14 18:13:27 +0900

    [dev] 가챠 티켓 상단 ui 추가

---
+++ Assets/_Project/Scripts/UI/TopCurrencyAndMenuBar/TopPanels/TopPanel_CTicket.cs
@@ -0,0 +1,29 @@
+using Cysharp.Text;
+
+namespace CookApps.AutoBattler
+{
+    public class TopPanel_CTicket : TopPanelBase
+    {
+        public override TopPanelType PanelType => TopPanelType.C_Ticket;
+
+        private void Awake()
+        {
+            CTicketChanged(UserDataManager.Instance.UserWallet.CTicket);
+        }
+
+        private void OnEnable()
+        {
+            UserDataManager.OnCTicketChanged += CTicketChanged;
+        }
+
+        private void OnDisable()
+        {
+            UserDataManager.OnCTicketChanged -= CTicketChanged;
+        }
+
+        private void CTicketChanged(int cTicket)
+        {
+            currencyText.SetText(cTicket.ToString("N0"));
+        }
+    }
+}
