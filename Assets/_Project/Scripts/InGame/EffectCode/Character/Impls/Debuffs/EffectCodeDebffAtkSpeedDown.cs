d62a7d5cead21e23cfc87c4bea43ff4dfede32fc
sgyoo1 <sgyoo1@cookapps.com>
2024-06-27 15:08:53 +0900

    [fix] 스킬 폴리싱

--- Assets/_Project/Scripts/InGame/EffectCode/Character/Impls/Debuffs/EffectCodeDebffAtkSpeedDown.cs
+++ Assets/_Project/Scripts/InGame/EffectCode/Character/Impls/Debuffs/EffectCodeDebffAtkSpeedDown.cs
@@ -5,7 +5,7 @@ using CookApps.TeamBattle.Utility;
 using UnityEngine.Pool;
 
 [UseEffectCodeIds(CodeId)]
-public class EffectCodeDebffBuffAtkSpeedDown : EffectCodeBuffBase
+public class EffectCodeDebffAtkSpeedDown : EffectCodeBuffBase
 {
     private const int CodeId = (int)EffectCodeNameType.DEBUFF_ATK_SPEED_DOWN;
     private const BuffDebuffType buffDebuffType = BuffDebuffType.AttackSpeedDown;
