feea3094a25079ee61f3aee69090aabf2d777c8f
JiyunHyun <jyhyun@cookapps.com>
2024-07-03 16:55:46 +0900

    [build] 포스트 프로세스 빌드 수정

--- Assets/Editor/PostprocessBuild.cs
+++ Assets/Editor/PostprocessBuild.cs
@@ -10,6 +10,26 @@ public class PostprocessBuild : IPostprocessBuild
     {
         if (report.Target == BuildTarget.iOS) // Check if the build is for iOS
         {
+            string xcodeProjectPath =  report.OutputPath + "/Unity-iPhone.xcodeproj/project.pbxproj";
+
+            PBXProject xcodeProject = new PBXProject();
+            xcodeProject.ReadFromFile(xcodeProjectPath);
+
+            string unityMainTargetGuid  = xcodeProject.GetUnityMainTargetGuid();
+            string unityFrameworkTargetGuid = xcodeProject.GetUnityFrameworkTargetGuid();
+
+            // IOS 릴리즈 시 FrameWork 오류로 인한 세팅
+            xcodeProject.SetBuildProperty(unityMainTargetGuid, "ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES", "YES");
+            xcodeProject.SetBuildProperty(unityFrameworkTargetGuid, "ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES", "NO");
+            xcodeProject.SetBuildProperty(unityMainTargetGuid, "ENABLE_MODULE_VERIFIER" , "NO");
+            xcodeProject.SetBuildProperty(unityFrameworkTargetGuid, "ENABLE_MODULE_VERIFIER" , "NO");
+
+            // IOS 빌드 시 오류로 인한 세팅
+            xcodeProject.SetBuildProperty(unityMainTargetGuid, "ENABLE_BITCODE", "NO");
+            xcodeProject.SetBuildProperty(unityFrameworkTargetGuid , "ENABLE_BITCODE", "NO");
+
+            xcodeProject.WriteToFile(xcodeProjectPath);
+
             string plistPath = report.OutputPath + "/Info.plist";
 
             PlistDocument plist = new PlistDocument(); // Read Info.plist file into memory
