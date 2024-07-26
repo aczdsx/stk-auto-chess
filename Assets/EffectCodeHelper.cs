7495bf8aafd004fd6a6bdd6503df485e04da56f7
sgyoo1 <sgyoo1@cookapps.com>
2024-07-26 22:30:05 +0900

    [dev] 이뮨 개발, 버프 디버프 개발

--- Assets/EffectCodeHelper.cs
+++ Assets/EffectCodeHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using CookApps.AutoBattler;
 using CookApps.BattleSystem;
 using CookApps.TeamBattle;
@@ -7,7 +8,37 @@ public static class EffectCodeHelper
 {
     public static void AddOrMergeEffectCode(EffectCodeNameType effectCodeNameType, CharacterController targetCharacter, Span<double> stats, IEffectCodeSource source)
     {
-        var effectCodeInfo = new EffectCodeInfo((long)effectCodeNameType, 0, stats);
-        targetCharacter.GetEffectCodeContainer().AddOrMergeEffectCode(effectCodeInfo, source);
+        HashSet<EffectCodeNameType> immuneTypes = new HashSet<EffectCodeNameType>
+        {
+            EffectCodeNameType.TILE_BURN,
+            EffectCodeNameType.STUN,
+            EffectCodeNameType.KNOCKBACK,
+            EffectCodeNameType.AIRBORNE,
+            EffectCodeNameType.MISA_RESTRAINT,
+            EffectCodeNameType.CHAPTER_FIRE,
+            EffectCodeNameType.CHAPTER_ICE,
+            EffectCodeNameType.CHAPTER_LANDMINE,
+            EffectCodeNameType.CHAPTER_SANDSTORM,
+            EffectCodeNameType.CHAPTER_RANDOM_MOVE,
+            EffectCodeNameType.DEBUFF_ATK_SPEED_DOWN,
+            EffectCodeNameType.DEBUFF_COOL_DOWN_SPEED_PERCENT_DOWN,
+            EffectCodeNameType.DEBUFF_AD_PERCENT_DOWN,
+            EffectCodeNameType.DEBUFF_FIRE,
+            EffectCodeNameType.DEBUFF_ICE,
+            EffectCodeNameType.DEBUFF_SILENCE,
+            EffectCodeNameType.DEBUFF_AD_DOWN,
+            EffectCodeNameType.DEBUFF_AIRBORNE,
+            EffectCodeNameType.DEBUFF_DEF_PERCENT_DOWN,
+            EffectCodeNameType.DEBUFF_HEAL_RATE_DOWN,
+        };
+        
+        bool isImmuneType = immuneTypes.Contains(effectCodeNameType);
+        bool hasImmuneBuff = targetCharacter.HasBuffDebuffType(BuffDebuffType.Immune) && isImmuneType;
+        
+        if (!hasImmuneBuff)
+        {
+            var effectCodeInfo = new EffectCodeInfo((long)effectCodeNameType, 0, stats);
+            targetCharacter.GetEffectCodeContainer().AddOrMergeEffectCode(effectCodeInfo, source);
+        }
     }
 }
