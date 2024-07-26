8f10c3e842f7c2906ed839b8d0d246cc1d5920f4
JiyunHyun <jyhyun@cookapps.com>
2024-07-26 18:24:42 +0900

    [dev] 아레나 시즌 보상 레이어 작업

--- Assets/_Project/Scripts/UI/Popup/Layers/PVPSeasonRewardLayer.cs
+++ Assets/_Project/Scripts/UI/Popup/Layers/PVPSeasonRewardLayer.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using CookApps.TeamBattle;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
-public class PVPSeasonRewardLayer : MonoBehaviour
+namespace CookApps.AutoBattler
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PVPSeasonRewardLayer : CachedMonoBehaviour
     {
+        [Header("Season Info List")]
+        [SerializeField] private ScrollRect _seasonInfoScrollRect;
+        [SerializeField] private GameObject _seasonInfoSlotObject;
         
-    }
+        private ArenaMainPopup _parentPopup;
 
-    // Update is called once per frame
-    void Update()
-    {
+        public void InitLayer(ArenaMainPopup parent)
+        {
+            _parentPopup = parent;
+
+            CreateSeasonScrollList();
+        }
         
+        public void RefreshLayer()
+        {
+            
+        }
+
+        private void CreateSeasonScrollList()
+        {
+            ClearLayer();
+
+            var tierDataList = SpecDataManager.Instance.GetPVPTierDataList(RankingType.SCORE);
+            tierDataList = tierDataList.OrderBy(data => data.order).ToList();
+            
+            foreach (var tierData in tierDataList)
+            {
+                GameObject newSlotObject = Instantiate(_seasonInfoSlotObject, _seasonInfoScrollRect.content);
+                var seasonInfoSlot = newSlotObject.GetComponent<ArenaSeasonRewardSlot>();
+                    
+                seasonInfoSlot?.InitSlot(tierData);
+            }
+
+            _seasonInfoScrollRect.horizontalNormalizedPosition = 0;
+        }
+        
+        public void ClearLayer()
+        {
+            BMUtil.RemoveChildObjects(_seasonInfoScrollRect.content);
+        }
     }
 }
