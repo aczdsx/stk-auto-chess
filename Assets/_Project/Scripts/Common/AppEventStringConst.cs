1038f007cdb5623d8aaf4af0eae197a2f9f1e807
JiyunHyun <jyhyun@cookapps.com>
2024-08-22 18:48:03 +0900

    [fix] 앱이벤트 파라미턴 컨벤션 수정

--- Assets/_Project/Scripts/Common/AppEventStringConst.cs
+++ Assets/_Project/Scripts/Common/AppEventStringConst.cs
@@ -2,44 +2,44 @@
 
 public static class AppEventStringConst
 {
-    public const string USER_ID = "USER_ID";
-    public const string PLAYER_ID = "PLAYER_ID";
-    public const string NICKNAME = "NICKNAME";
-    public const string SERVER = "SERVER";
-    public const string TOTAL_PLAY_TIME = "TOTAL_PLAY_TIME";
-    public const string DAILY_VISIT_COUNT = "DAILY_VISIT_COUNT";
-    public const string SINCE_JOIN_DATE = "SINCE_JOIN_DATE";
-    public const string USER_INSTALL_DATE = "USER_INSTALL_DATE";
-    public const string BEST_STAGE = "BEST_STAGE";
-    public const string BEST_MISSION = "BEST_MISSION";
-    public const string USER_POWER = "USER_POWER";
-    public const string USER_LEVEL = "USER_LEVEL";
-    public const string USER_GRADE = "USER_GRADE";
-    public const string USER_STAR_AMOUNT = "USER_STAR_AMOUNT";
-    public const string USER_ENERGY_AMOUNT = "USER_ENERGY_AMOUNT";
+    public const string USER_ID = "user_id";
+    public const string PLAYER_ID = "player_id";
+    public const string NICKNAME = "nickname";
+    public const string SERVER = "server";
+    public const string TOTAL_PLAY_TIME = "total_play_time";
+    public const string DAILY_VISIT_COUNT = "daily_visit_count";
+    public const string SINCE_JOIN_DATE = "since_join_date";
+    public const string USER_INSTALL_DATE = "user_install_date";
+    public const string BEST_STAGE = "best_stage";
+    public const string BEST_MISSION = "best_mission";
+    public const string USER_POWER = "user_power";
+    public const string USER_LEVEL = "user_level";
+    public const string USER_GRADE = "user_grade";
+    public const string USER_STAR_AMOUNT = "user_star_amount";
+    public const string USER_ENERGY_AMOUNT = "user_energy_amount";
     
-    public const string TYPE = "TYPE";
-    public const string STAGE_ID = "STAGE_ID";
-    public const string PLAY_TIME = "PLAY_TIME";
-    public const string SQUAD_COUNT = "SQUAD_COUNT";
-    public const string POWER = "POWER";
-    public const string ENEMY_POWER = "ENEMY_POWER";
-    public const string RESULT = "RESULT";
-    public const string REASON = "REASON";
-    public const string CLEAR_CONDITION = "CLEAR_CONDITION";
-    public const string COMMANDER_SKILL = "COMMANDER_SKILL";
-    public const string DECK = "DECK";
+    public const string TYPE = "type";
+    public const string STAGE_ID = "stage_id";
+    public const string PLAY_TIME = "play_time";
+    public const string SQUAD_COUNT = "squad_count";
+    public const string POWER = "power";
+    public const string ENEMY_POWER = "enemy_power";
+    public const string RESULT = "result";
+    public const string REASON = "reason";
+    public const string CLEAR_CONDITION = "clear_condition";
+    public const string COMMANDER_SKILL = "commander_skill";
+    public const string DECK = "deck";
     
-    public const string ENEMY_PLAYER_ID = "ENEMY_PLAYER_ID";
-    public const string ENEMY_POINT = "ENEMY_POINT";
-    public const string ENEMY_GRADE = "ENEMY_GRADE";
-    public const string ENEMY_DECK = "ENEMY_DECK";
-    public const string ENEMY_DECK_POWER = "ENEMY_DECK_POWER";
+    public const string ENEMY_PLAYER_ID = "enemy_player_id";
+    public const string ENEMY_POINT = "enemy_point";
+    public const string ENEMY_GRADE = "enemy_grade";
+    public const string ENEMY_DECK = "enemy_deck";
+    public const string ENEMY_DECK_POWER = "enemy_deck_power";
     
-    public const string SEASON = "SEASON";
-    public const string GRADE = "GRADE";
-    public const string POINT = "POINT";
-    public const string RANKING = "RANKING";
+    public const string SEASON = "season";
+    public const string GRADE = "grade";
+    public const string POINT = "point";
+    public const string RANKING = "ranking";
     
-    public const string GUIDE_MISSION_ID = "GUIDE_MISSION_ID";
+    public const string GUIDE_MISSION_ID = "guide_mission_id";
 }
