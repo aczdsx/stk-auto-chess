9f084c80d19dcea1a1c29ce8738d3bf0b9f354f7
sungwoonyang <swyang@cookapps.com>
2026-01-12 20:00:07 +0900

    커맨드센터 로컬코드제거

--- Assets/_Project/Scripts/UI/Popup/Elpis/CommandCenter/ElpisCommandCenterPopup.cs
+++ Assets/_Project/Scripts/UI/Popup/Elpis/CommandCenter/ElpisCommandCenterPopup.cs
@@ -249,7 +249,7 @@ namespace CookApps.AutoBattler
                     Debug.LogWarning("다음 업그레이드가 없습니다");
                 return;
             }
-            
+
             if(currentCoreAmount < requiredCoreForUpgrade)
                 return;
 
@@ -257,18 +257,16 @@ namespace CookApps.AutoBattler
 
             try
             {
-                // 업그레이드 실행
                 var commandCenter = dataBridge.GetFacilityByType(ElpisFacilityType.FacilityTypeCommandCenter);
                 var resp = await NetManager.Instance.Elpis.UpgradeFacilityAsync((int)commandCenter.BuildId);
-                //10130102 에러뜸 : facility not invalid 에러임. 걍 로컬로 변경해서 함
+                if (!resp.IsSuccess)
+                {
+                    Debug.LogError($"Command Center Upgrade Error:{resp.Exception}");
+                    return;
+                }
 
-                commandCenter.Level++; //TODO : 서버 위 에러 없어지면 삭제
                 currentElpisLevel++;
 
-                // 해금된 시설 데이터 추가
-                lobbyMain.RefreshUnlockedFacilities();
-
-                // SubBlock 확장 애니메이션 (레벨 2, 3에서만 실행)
                 var subBlockIndex = currentElpisLevel - 2; // 레벨2 -> 인덱스0, 레벨3 -> 인덱스1
                 if (subBlockIndex >= 0 && subBlockIndex <= 1)
                 {
@@ -293,20 +291,12 @@ namespace CookApps.AutoBattler
                     await cameraController.ZoomAsync(offsetZoom, 0.3f);
                 }
 
-                // UI 슬롯 갱신 (서브블록 애니메이션 후)
-                lobbyMain.RefreshWorldInteractionSlots(commandCenter.Level);
-                
-                // 새 데이터로 UI 갱신
                 currentCoreAmount = (int)inventoryDataBridge.GetCurrency(IdMap.Item.엘피스코어);
                 LoadElpisData();
                 UpdateUI();
 
-                //결과 팝업
-
                 var resultPopup = await SceneUILayerManager.Instance.PushUILayerAsync<ElpisCommandCenterResultPopup>(currentBenefits);
                 await resultPopup.WaitForExit();
-                UnityEngine.Debug.Log("resultPopup exit");
-
                 await PlayEnterAnimationAsync();
             }
             finally
