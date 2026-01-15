dda9d3ee642bfc19992a0c6e2e876ed78ac242e0
KHKIM2-PC\sschoi <khkim2@cookapps.com>
2026-01-16 00:42:36 +0900

    [fix] 필리아 이펙트 위치 수정

--- Assets/_Project/Scripts/InGame/EffectCode/Character/Impls/Skills/EffectCodeSkill217433303.cs
+++ Assets/_Project/Scripts/InGame/EffectCode/Character/Impls/Skills/EffectCodeSkill217433303.cs
@@ -119,7 +119,7 @@ public partial class EffectCodeSkill217433303 : EffectCodeCharacterBase
         var hitEffect = InGameVfxManager.Instance.AddInGameVfx(_specSkill.skill_vfxs[2],
             owner.SkillRootTransformFollowable);
         var direction = (owner.Target.CurrentTile.View.CachedTr.position - owner.CurrentTile.View.CachedTr.position).normalized;
-        hitEffect.CachedTr.rotation = Quaternion.LookRotation(direction);
+        hitEffect.CachedTr.rotation = Quaternion.LookRotation(direction) * Quaternion.Euler(0, -90, 0);;
 
         var damage = owner.CalculateDamageAmount(owner.AD * _powerRate, 0, _targetCharacter, codeId, true);
 
