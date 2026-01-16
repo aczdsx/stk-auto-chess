2f970617c9b889cb8783675c6c26c5bf62d78b40
sjhong <sjhong1@cookapps.com>
2026-01-16 10:37:21 +0900

    [fix] : 그로기 -> IDLE 버그 수정

---
+++ Assets/_Project/Scripts/InGame/EffectCode/Character/Impls/Skills/EffectCodeSkillPrologue.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using CookApps.AutoBattler;
+using CookApps.Obfuscator;
+using CookApps.BattleSystem;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using CharacterController = CookApps.BattleSystem.CharacterController;
+
+/// <summary>
+/// 프롤로그_유니
+/// </summary>
+[UseEffectCodeIds(295250010)]
+public partial class EffectCodeSkill295250010 : EffectCodeSkill215252102
+{
+    
+}
+
+
+/// <summary>
+/// 프롤로그_필리아
+/// </summary>
+[UseEffectCodeIds(295530011)]
+public partial class EffectCodeSkill295530011 : EffectCodeSkill215532401
+{
+    
+}
+
+
+
+/// <summary>
+/// 프롤로그_아트레시아
+/// </summary>
+[UseEffectCodeIds(297510012)]
+public partial class EffectCodeSkill297510012 : EffectCodeSkill217513401
+{
+    
+}
+
+
+
+/// <summary>
+/// 프롤로그_클레이
+/// </summary>
+[UseEffectCodeIds(297550013)]
+public partial class EffectCodeSkill297550013 : EffectCodeSkill217553404
+{
+    
+}
+
+
+
+/// <summary>
+/// 프롤로그_오데트
+/// </summary>
+[UseEffectCodeIds(297610014)]
+public partial class EffectCodeSkill297610014 : EffectCodeSkill217613501
+{
+    
+}
+
+
+/// <summary>
+/// 프롤로그_마리에
+/// </summary>
+[UseEffectCodeIds(297560015)]
+public partial class EffectCodeSkill297560015 : EffectCodeSkill217563405
+{
+    
+}
