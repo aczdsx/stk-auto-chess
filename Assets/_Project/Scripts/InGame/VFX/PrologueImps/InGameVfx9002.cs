4736f640e34e2b7eb7d478be0b9b3b73a259155b
sjhong <sjhong1@cookapps.com>
2026-01-16 15:29:03 +0900

    [dev] 2단계 타이밍

---
+++ Assets/_Project/Scripts/InGame/VFX/PrologueImps/InGameVfx9002.cs
@@ -0,0 +1,86 @@
+using System;
+using CookApps.AutoBattler;
+using CookApps.TeamBattle;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace CookApps.BattleSystem
+{
+    public class InGameVfx9002 : InGameVfx
+    {
+        [SerializeField] private GameObject _orbGroup;
+        [SerializeField] private GameObject _explosion;
+        [SerializeField] private AnimationCurve _curve;
+        [SerializeField] private float _duration = 1f;
+
+        private float _elapsedTime;
+        private bool _isPlaying = true;
+
+        public float CurrentValue { get; private set; }
+
+        public override void Initialize(bool isFlipX, InGameVfxMovementBase movementBase = null)
+        {
+            base.Initialize(isFlipX, movementBase);
+            _elapsedTime = 0f;
+            MyDebug.Log($"_isPlaying : {_isPlaying}");
+            CurrentValue = _curve?.Evaluate(0f) ?? 0f;
+        }
+
+        public override void ManagedUpdate(float dt)
+        {
+            base.ManagedUpdate(dt);
+            if (!_isPlaying){
+                MyDebug.Log($"_isPlaying : {_isPlaying}");
+                return;}
+            else
+            {
+                MyDebug.Log($"_elapsedTime : {_elapsedTime} \ndt : {dt}");
+            }
+
+            _elapsedTime += dt;
+            float normalizedTime = Mathf.Clamp01(_elapsedTime / _duration);
+            CurrentValue = _curve?.Evaluate(normalizedTime) ?? 0f;
+
+            OnCurveUpdate(CurrentValue);
+
+            if (_elapsedTime >= _duration)
+            {
+                _isPlaying = false;
+                OnCurveComplete();
+            }
+        }
+
+        protected virtual void OnCurveUpdate(float value)
+        {
+            // 커브 값에 따라 업데이트 처리
+            MyDebug.Log($"OnCurveUpdate : {value}");
+            _orbGroup.transform.localPosition = Vector3.up * value;
+        }
+
+        protected virtual void OnCurveComplete()
+        {
+            MyDebug.Log($"protected virtual void OnCurveComplete()");
+            // 커브 완료 시 처리
+            _orbGroup.SetActive(false);
+            _explosion.SetActive(true);
+        }
+
+        public override void Clear()
+        {
+            base.Clear();
+            _elapsedTime = 0f;
+            _isPlaying = false;
+        }
+
+        public void Play()
+        {
+            _elapsedTime = 0f;
+            _isPlaying = true;
+        }
+
+        public void Stop()
+        {
+            _isPlaying = false;
+        }
+    }
+}
