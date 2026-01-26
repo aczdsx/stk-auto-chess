06ed127d0cf4b75da5885e625c2db0af41787a0d
Taewoo Han <twhan@cookapps.com>
2026-01-26 17:49:48 +0900

    나니노벨 버그 수정

--- Assets/_Project/Scripts/UI/Common/UILayerPopupBase.cs
+++ Assets/_Project/Scripts/UI/Common/UILayerPopupBase.cs
@@ -5,6 +5,7 @@ namespace CookApps.AutoBattler
     /// <summary>
     /// UILayerType.Popup용 Base 클래스
     /// OnPostEnter/OnPostExit 시점에 튜토리얼 트리거 발동
+    /// TODO: TutorialManager에서 Event 받아서 처리하도록 리팩토링 필요
     /// </summary>
     public abstract class UILayerPopupBase : UILayer
     {
