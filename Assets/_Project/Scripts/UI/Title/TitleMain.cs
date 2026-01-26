833c1c2c13522d620f747fe81488003cdd3643e5
Taewoo Han <twhan@cookapps.com>
2026-01-26 15:39:58 +0900

    BadgeManager 초기화 위치 이동

--- Assets/_Project/Scripts/UI/Title/TitleMain.cs
+++ Assets/_Project/Scripts/UI/Title/TitleMain.cs
@@ -60,6 +60,9 @@ namespace CookApps.AutoBattler
 
             await UniTask.WhenAll(tasks);
 
+            // 뱃지 시스템 초기화
+            BadgeManager.Instance.Initialize();
+
             await ConnectWithServer();
 
             // Addressables 다운로드 체크 및 진행
@@ -102,9 +105,6 @@ namespace CookApps.AutoBattler
 
             // 앱이벤트 전송
             AppEventManager.Instance.Login();
-
-            // 뱃지 시스템 초기화
-            BadgeManager.Instance.Initialize();
 
             // 서버 데이터 초기화 (Elpis 포함)
             await NetManager.Instance.InitializeAsync();
