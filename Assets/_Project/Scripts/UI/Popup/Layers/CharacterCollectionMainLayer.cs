69932ff3de390e953f322d71b738f4ec77198ee5
KHKIM2-PC\sschoi <khkim2@cookapps.com>
2026-03-04 19:50:18 +0900

    [fix] 컬렉션 팝업 새로운 프리팹에 맞게 적용 -> grid에서 tableview로 컴포넌트 변경

--- Assets/_Project/Scripts/UI/Popup/Layers/CharacterCollectionMainLayer.cs
+++ Assets/_Project/Scripts/UI/Popup/Layers/CharacterCollectionMainLayer.cs
@@ -1,10 +1,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using CookApps.TeamBattle;
+using CookApps.TeamBattle.UI;
 using CookApps.TeamBattle.UIManagements;
 using R3;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace CookApps.AutoBattler
 {
@@ -13,13 +13,14 @@ namespace CookApps.AutoBattler
         [SerializeField] private CAButton _backButton;
 
         [Space(10)]
-        [SerializeField] private ScrollRect _characterScrollRect;
-        [SerializeField] private GameObject _characterCardSlotObject;
+        [SerializeField] private TableView _characterTableView;
+        [SerializeField] private CharacterCardSlot _characterCardSlotPrefab;
 
         private SynergyType _currentMainLayerTabType = SynergyType.NORMAL;
 
         private List<CharacterInfo> _totalCharacterList;      // 전체 캐릭터 리스트
-        private List<CharacterCardSlot> _characterCardSlotList = new List<CharacterCardSlot>();
+        private List<CharacterInfo> _filteredCharacterList = new List<CharacterInfo>();
+        private TableViewController<CharacterInfo, CharacterCardSlot> _characterController;
 
         [SerializeField]
         private GameObject _guideObj;
@@ -62,7 +63,6 @@ namespace CookApps.AutoBattler
             _totalCharacterList = _totalCharacterList.OrderByDescending(data => ServerDataManager.Instance.Character.HasCharacter(data.id))
                 .ThenByDescending(data => data.id).ToList();
 
-
             var guideMission = ServerDataManager.Instance.GuideMission;
             var _specGuideMissionData = SpecDataManager.Instance.GuideMissionInfo.Get((int)guideMission.GuideMissionId);
             bool isGuide = false;
@@ -73,57 +73,57 @@ namespace CookApps.AutoBattler
 
             var isTutorial = TutorialManager.Instance.IsTutorial;
 
-            foreach (var characterData in _totalCharacterList)
-            {
-                GameObject newCardObject = Instantiate(_characterCardSlotObject, _characterScrollRect.content);
-                CharacterCardSlot slot = newCardObject.GetComponent<CharacterCardSlot>();
-                slot.SetCharcacterSlot(characterData, _characterCardSlotList);
+            _filteredCharacterList.Clear();
+            _filteredCharacterList.AddRange(_totalCharacterList);
 
-                if (isGuide && _guideObj != null)
+            _characterController = _characterTableView.CreateController<CharacterInfo, CharacterCardSlot>()
+                .WithData(_filteredCharacterList)
+                .WithCellPrefab(_characterCardSlotPrefab.gameObject)
+                .WithCellSize(_characterCardSlotPrefab.GetComponent<RectTransform>().rect.size)
+                .OnBind((cell, characterData, index) =>
                 {
-                    if (_specGuideMissionData.sub_key == characterData.id)
+                    cell.SetCharcacterSlot(characterData, _filteredCharacterList);
+
+                    // 가이드 오브젝트 배치
+                    if (isGuide && _guideObj != null && _specGuideMissionData.sub_key == characterData.id)
                     {
-                        _guideObj.transform.parent = newCardObject.transform;
+                        _guideObj.transform.SetParent(cell.transform, false);
                         _guideObj.transform.localPosition = Vector3.zero;
                     }
-                }
 
-                // 튜토리얼 모드일 때 TutorialTarget 동적 등록
-                if (isTutorial)
+                    // 튜토리얼 모드일 때 TutorialTarget 동적 등록
+                    if (isTutorial)
+                    {
+                        var tutorialTarget = cell.GetComponent<TutorialTarget>()
+                                             ?? cell.gameObject.AddComponent<TutorialTarget>();
+                        tutorialTarget.SetTargetId($"CharacterCardSlot_{characterData.id}");
+                    }
+                })
+                .OnCellReleased((index, cell) =>
                 {
-                    var tutorialTarget = newCardObject.GetComponent<TutorialTarget>()
-                                         ?? newCardObject.AddComponent<TutorialTarget>();
-                    tutorialTarget.SetTargetId($"CharacterCardSlot_{characterData.id}");
-                }
-
-                _characterCardSlotList.Add(slot);
-            }
-
-            _characterScrollRect.verticalNormalizedPosition = 1;
+                    cell.ClearCardSlot();
+                })
+                .Build();
         }
 
         private void FilterCharacterList(SynergyType targetType)
         {
-            _characterCardSlotList.ForEach(slot =>
-            {
-                if (targetType == SynergyType.NORMAL)
-                {
-                    slot.gameObject.SetActive(true);
-                }
-                else
-                {
-                    slot.gameObject.SetActive((int)slot.SpecCharacterData.character_element_type == (int)targetType);
-                }
-            });
+            _filteredCharacterList.Clear();
 
-            _characterScrollRect.verticalNormalizedPosition = 1;
+            if (targetType == SynergyType.NORMAL)
+                _filteredCharacterList.AddRange(_totalCharacterList);
+            else
+                _filteredCharacterList.AddRange(
+                    _totalCharacterList.Where(c => c.character_element_type == targetType));
+
+            _characterTableView.RefreshAll(resetPos: true);
         }
 
         private void ClearList()
         {
-            _characterCardSlotList.Clear();
-
-            BMUtil.RemoveChildObjects(_characterScrollRect.content);
+            _characterController?.Detach();
+            _characterController = null;
+            _filteredCharacterList.Clear();
         }
 
         private void OnClickBackButton()
